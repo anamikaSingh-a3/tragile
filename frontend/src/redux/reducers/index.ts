336bee270fd202ea8d63be841a16c28db0fa959e
Anamika Singh <anamikasingh@Anamikas-MacBook-Pro.local>
2021-12-02 08:29:19 +0530

    Added: reducer

--- frontend/src/redux/reducers/index.ts
+++ frontend/src/redux/reducers/index.ts
@@ -3,12 +3,16 @@ import activeWorkspaceReducer from './activeWorkspaceReducer'
 import workspaceReducer from './workspaceReducer'
 import activeWorkspaceBoardReducer from './activeWorkspaceBoardsReducer'
 import activeBoardListReducer from './activeBoardListReducer'
+import activeListReducer from './activeListReducer'
+import listReducer from './listReducer'
 
 export default combineReducers({
   workspaces: workspaceReducer,
+  list: listReducer,
   activeWorkspace: activeWorkspaceReducer,
   activeWorkspaceBoard: activeWorkspaceBoardReducer,
   activeBoardList : activeBoardListReducer,
+  activeList: activeListReducer,
 
 })
 
---
+++ frontend/src/redux/reducers/listReducer.ts
@@ -0,0 +1,29 @@
+import { IList } from '../interfaces'
+import { ACTIVE_LIST, ADD_LIST } from '../types'
+
+const initialState: IList[] = []
+
+const addListLogic = (state: IList[], payload: any) => {
+  if (payload.length >= 1)
+    return [...state, ...payload]
+  else
+    return [...state, payload]
+}
+
+const listReducer = (
+  state = initialState,
+  action: { type: string; payload: IList[] }
+) => {
+  switch (action.type) {
+    case ADD_LIST: {
+      return addListLogic(state, action.payload)
+    }
+    case ACTIVE_LIST: {
+      return action.payload
+    }
+    default:
+      return state
+  }
+}
+
+export default listReducer
