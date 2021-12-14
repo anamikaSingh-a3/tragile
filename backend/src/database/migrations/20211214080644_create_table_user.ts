c1465c4618beb46fe1c73c699fa05309075d07bd
Anamika <anamika.singh@block8.com>
2021-12-14 14:46:57 +0530

    feat(bed): migrate down

--- backend/src/database/migrations/20211214080644_create_table_user.ts
+++ backend/src/database/migrations/20211214080644_create_table_user.ts
@@ -1,16 +1,17 @@
 import { Knex } from 'knex'
 
-export async function up (knex: Knex): Promise<void> {
+export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', table => {
     table.increments('user_id')
     table.string('name').notNullable()
-    table.string('username').notNullable()
+    table.string('username')
+    table.string('bio')
     table.string('email').notNullable()
     table.string('password').notNullable()
     table.timestamps(true, true)
   })
 }
 
-export async function down (knex: Knex): Promise<void> {
+export async function down(knex: Knex): Promise<void> {
   return knex.schema.dropTableIfExists('users')
 }
