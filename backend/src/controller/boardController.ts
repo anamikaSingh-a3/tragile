f3e97823a0462ad217d95564133400da22737298
Anamika <anamika.singh@block8.com>
2021-12-15 10:42:36 +0530

    feat(bed): added response object to board api

--- backend/src/controller/boardController.ts
+++ backend/src/controller/boardController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { IBoard } from 'tragile-board'
+import { ITragileResponse } from 'tragile-response'
 import { pool } from '../db'
 import {
   boardSchema,
@@ -7,6 +8,12 @@ import {
   getWorkspaceBoardSchema
 } from '../schema/boardSchema'
 import logger from '../utility/logger'
+
+const response: ITragileResponse = {
+  statusCode: 0,
+  payload: {},
+  message: "Something went wrong!"
+}
 
 export const createBoard = async (req: Request, res: Response) => {
   logger.info('In create board API')
@@ -22,16 +29,23 @@ export const createBoard = async (req: Request, res: Response) => {
       'INSERT INTO board (board_id,title,visibility,workspace) VALUES ($1,$2,$3,$4) RETURNING *',
       [board.board_id, board.title, board.visibility, board.workspaceId]
     )
-    res.status(200).send(newBoard.rows[0])
+    response.statusCode = 201
+    response.payload = newBoard.rows
+    response.message = "Board created"
     logger.info('New board created')
+    res.status(response.statusCode).send(response)
   } catch (error) {
-    res.status(401).send(error)
+    response.statusCode = 400
+    response.payload = {}
+    response.message = "Board could not be created!"
     logger.error(error)
+    res.status(response.statusCode).send(response)
   }
 }
 
 export const getBoard = async (req: Request, res: Response) => {
   logger.info('In get board API')
+
   try {
     const board_id = req.params.board_id
     await getBoardSchema.isValid(board_id)
@@ -40,18 +54,29 @@ export const getBoard = async (req: Request, res: Response) => {
     ])
 
     if (board.rowCount < 1) {
+      response.statusCode = 404
+      response.payload = {}
+      response.message = "No board found"
       logger.warn('No board found')
-      return res.status(400).send('No board found')
+      res.status(response.statusCode).send(response)
     }
-    res.status(200).send(board.rows[0])
-    logger.info('Board fetched successfully')
+    else {
+      response.statusCode = 200
+      response.payload = board.rows
+      response.message = 'Board fetched successfully'
+      res.status(response.statusCode).send(response)
+      logger.info('Board fetched successfully')
+    }
   } catch (error) {
-    res.status(401).send(error)
+    response.statusCode = 400
+    res.status(response.statusCode).send(response)
+    logger.error(error)
   }
 }
 
 export const getWorkspaceBoard = async (req: Request, res: Response) => {
-  logger.info('In get workspace API')
+  logger.info('In get workspace board API')
+
   try {
     const workspace_id = req.params.workspace_id
     await getWorkspaceBoardSchema.isValid(workspace_id)
@@ -60,13 +85,22 @@ export const getWorkspaceBoard = async (req: Request, res: Response) => {
     ])
     if (board.rowCount < 1) { 
       logger.warn('No Board found')
-      return res.status(400).send([])
+      response.statusCode = 404
+      response.payload = {}
+      response.message = "No Board found"
+      res.status(response.statusCode).send(response)
     }
-    logger.info('Boards fetched successfully')
-    res.status(200).send(board.rows)
+    else {
+      logger.info('Boards fetched successfully')
+      response.statusCode = 200,
+      response.payload = board.rows
+      response.message = "Boards fetched successfully"
+      res.status(response.statusCode).send(response)
+    }
   } catch (error) {
     logger.error(error)
-    res.status(401).send(error)
+    response.statusCode = 400
+    res.status(response.statusCode).send(response)
   }
 }
 
@@ -74,10 +108,23 @@ export const getAllBoard = async (req: Request, res: Response) => {
   logger.info('In get all board API')
   try {
     const boards = await pool.query('SELECT * FROM board')
-    res.status(200).send(boards.rows)
-    logger.info('Board successfully fetched')
+    if (boards.rowCount < 1) {
+      response.statusCode = 404
+      response.payload = {}
+      response.message = "No board found"
+      logger.warn('No board found')
+      res.status(response.statusCode).send(response)
+    }
+    else {
+      response.statusCode = 200
+      response.payload = boards.rows
+      response.message = 'Board fetched successfully'
+      res.status(response.statusCode).send(response)
+      logger.info('Board fetched successfully')
+    }
   } catch (error) {
+    response.statusCode = 400
+    res.status(response.statusCode).send(response)
     logger.error(error)
-    res.status(401).send(error)
   }
 }
