e5bd14479b483df67cfbb959cad24e5c4c0fe3d1
Anamika <anamika.singh@block8.com>
2021-12-28 14:08:45 +0530

    feat(backend): Added: PM2 and updated status code

--- frontend/src/redux/thunk/userThunk/signInThunk.ts
+++ frontend/src/redux/thunk/userThunk/signInThunk.ts
@@ -8,12 +8,12 @@ export const signInThunk = (email: string, password: string) => async (dispatch:
     const user = { email, password }
     try {
         const response = await axios.post(`${userApi}/signIn`, user)
-        if (response.status === 202) {
+        if (response.status === 200) {
             localStorage.setItem("token", JSON.stringify(response.data.payload.token));
             dispatch(addUser(response.data.payload))
             dispatch(messageAction(response.data.message))
         }
-        else if (response.status === 200) dispatch(messageAction(response.data.message))
+        else if (response.status === 401) dispatch(messageAction(response.data.message))
 
     } catch (error) {
         dispatch(messageAction('User could not be signed in'))
